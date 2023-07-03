1fa67c2fa7ab57009ec85dd03952dfd4f64b85c8
Samarth Zalte <52416785+samz905@users.noreply.github.com>
2023-07-03 12:12:42 +0530

    final structure updated

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import requests
 from datetime import datetime, timezone
-import logging
 
 load_dotenv()
 
@@ -62,28 +61,67 @@ function_descriptions = [
 
 
 def doc_creator(content):
+    """
+    Create documents from text content.
+
+    Args:
+        content (str): The input text content.
+
+    Returns:
+        list: The list of created documents.
+    """
+    # Initialize the text splitter
     text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=150)
+
+    # Split the content into chunks
     split_content = text_splitter.split_text(content)
 
+    # Split the first chunk by newline character
     split_content = split_content[0].split("\n")
 
+    # Create documents from the split content
     docs = text_splitter.create_documents(split_content)
 
     return docs
 
 
-def final_summary(content):
-    docs = doc_creator(content)
+def generate_summary(content):
+    """
+    Generate a summary of the given content.
 
-    llm = ChatOpenAI(model="gpt-3.5-turbo-0613", temperature=0.5)
+    Args:
+        content (str): The content to generate a summary from.
 
-    chain = load_summarize_chain(llm, chain_type="map_reduce")
-    summary = chain.run(docs)
+    Returns:
+        str: The generated summary.
+    """
+    # Create documents from content
+    documents = doc_creator(content)
+
+    # Initialize the ChatOpenAI model
+    model = ChatOpenAI(model="gpt-3.5-turbo-0613", temperature=0.5)
+
+    # Load and initialize the summarization chain
+    chain = load_summarize_chain(model, chain_type="map_reduce")
+
+    # Generate the summary using the chain
+    summary = chain.run(documents)
 
     return summary
 
 
-def short_summary(content):
+def generate_short_summary(content):
+    """
+    Generate a short summary of the given content.
+
+    Args:
+        content (str): The content to summarize.
+
+    Returns:
+        str: The generated short summary.
+    """
+
+    # Define the prompt template
     prompt_template = """Write a concise summary in less than 500 characters of the text given below. If it is a 
     newsletter, refer to it as a newsletter. If it isn't a newsletter, simply make summary say "This isn't a newsletter".
     If it is a newsletter, the summary should be less than 500 characters long and refer to the original text as a 
@@ -94,65 +132,85 @@ def short_summary(content):
 
     SUMMARY OF NEWSLETTER IN LESS THAN 500 CHARACTERS:"""
 
-    docs = doc_creator(content)
-
-    print(docs)
-
-    llm = ChatOpenAI(model="gpt-3.5-turbo-0613", temperature=0.5)
-
-    PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
-    chain = load_summarize_chain(llm, chain_type="stuff", prompt=PROMPT)
-    summary = chain.run(docs[:3])
+    # Generate the summary using the load_summarize_chain function
+    summary = load_summarize_chain(
+        ChatOpenAI(model=models[0], temperature=0.5),
+        chain_type="stuff",
+        prompt=PromptTemplate(template=prompt_template, input_variables=["text"])
+    ).run(doc_creator(content))
 
     return summary
 
 
 def summarise_newsletter(content):
-    short_sum = short_summary(content)
+    # Generate a short summary of the newsletter content
+    short_summary = generate_short_summary(content)
+    print(short_summary)
 
-    print(short_sum)
-
-    query_title=f"Please generate a title in less than 100 characters for the following newsletter summary content: {short_sum}"
+    # Prompt the user to generate a title for the summary content
+    query_title = f"Please generate a title in less than 100 characters for the following newsletter summary content: {short_summary}"
     messages_title = [{"role": "user", "content": query_title}]
 
+    # Generate the title using an AI model
     title = openai.ChatCompletion.create(
         model=models[0],
         messages=messages_title,
         temperature=0.5,
-        functions = function_descriptions,
-        function_call={
-            "name": "summary_title"
-        }
+        functions=function_descriptions,
+        function_call={"name": "summary_title"}
     )
 
+    # Extract the generated title from the AI response
     title_json = json.loads(title["choices"][0]["message"]["function_call"]["arguments"])
     title = title_json["title"]
 
-    final_sum = final_summary(content)
+    # Generate the final summary of the newsletter content
+    final_summary = generate_summary(content)
 
+    # Create a summary object with the title and final summary
     summary_object = {
         "title": title,
-        "summary": final_sum
+        "summary": final_summary
     }
 
     return summary_object
 
 
-def create_notion_page(data: dict):
-    create_url = "https://api.notion.com/v1/pages"
+def create_notion_page(data: dict) -> requests.Response:
+    """
+    Creates a new page in Notion using the provided data.
 
-    res = requests.post(create_url, headers=headers, data=json.dumps(data))
+    Args:
+        data (dict): The data to be sent in the request body.
 
-    print("Notion response:", res.json())
-
-    return res
+    Returns:
+        requests.Response: The response object containing the server's response to the request.
+    """
+    url = "https://api.notion.com/v1/pages"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer YOUR_NOTION_API_TOKEN"
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response
 
 
 def send_to_notion(summary_obj: dict):
+    """
+    Sends the given summary object to Notion.
+
+    Args:
+        summary_obj (dict): The summary object to send.
+
+    """
+    # Check if the summary object is not empty
     if summary_obj:
+        # Extract the required values from the summary object
         title = summary_obj["title"]
         summary = summary_obj["summary"]
         published_date = datetime.now().astimezone(timezone.utc).isoformat()
+        
+        # Create the data payload to send to Notion
         data = {
             "parent": {"database_id": NOTION_DATABASE_ID},
             "properties": {
@@ -177,6 +235,7 @@ def send_to_notion(summary_obj: dict):
             ]
         }
 
+        # Call the create_notion_page function with the data payload
         create_notion_page(data)
 
 
@@ -184,35 +243,52 @@ class Email(BaseModel):
     from_email: str
     content: str
 
+
 @app.get("/")
 def read_root():
+    """
+    Get request handler for the root endpoint.
+    
+    Returns:
+        dict: A dictionary with the message "Hello" and "World".
+    """
     return {"Hello": "World"}
+
 
 @app.post("/")
 def email_to_notion(email: Email):
+    """
+    This function handles an incoming email and sends it to Notion.
+    
+    Args:
+        email (Email): The email object containing the email content.
+    """
+    # Get the email content
     content = email.content
-
-    summary = ""
-    summary = short_summary(content)
-
-    query = f"Please check if this email is a newsletter or not: {summary} "
-
-    messages = [{"role": "user", "content": query}]
-
+    
+    # Generate a short summary of the email content
+    summary = generate_short_summary(content)
+    
+    # Create a query to ask if the email is a newsletter or not
+    query = f"Please check if this email is a newsletter or not: {summary}"
+    
+    # Create messages for OpenAI chat completion
+    messages = [
+        {"role": "user", "content": query}
+    ]
+    
+    # Make a request to OpenAI chat completion
     response = openai.ChatCompletion.create(
         model=models[0],
         messages=messages,
-        functions = function_descriptions,
-        function_call={
-            "name": "categorise_email"
-        }
+        functions=function_descriptions,
+        function_call={"name": "categorise_email"}
     )
-
-    arguments = response.choices[0]["message"]["function_call"]["arguments"]
-    json_args = json.loads(arguments)
-
-    is_newsletter = json_args["is_newsletter"]
-
+    
+    # Extract the value of is_newsletter from the response
+    is_newsletter = json.loads(response.choices[0]["message"]["function_call"]["arguments"])["is_newsletter"]
+    
+    # If the email is a newsletter, summarize it and send to Notion
     if is_newsletter:
         summary_obj = summarise_newsletter(content)
         send_to_notion(summary_obj)
